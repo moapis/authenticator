4b73f6549c0aa88d3e27de41deac1207873cdf62
Tim Möhlmann <muhlemmer@gmail.com>
2019-12-02 23:07:59 +0200

    BSD-3-Clause license

--- server/migrations/00-00-00-First_version.sql
+++ server/migrations/00-00-00-First_version.sql
@@ -1,3 +1,7 @@
+-- Copyright (c) 2019, Mohlmann Solutions SRL. All rights reserved.
+-- Use of this source code is governed by a License that can be found in the LICENSE file.
+-- SPDX-License-Identifier: BSD-3-Clause
+
 -- +migrate Up
 
 create table jwt_keys (
