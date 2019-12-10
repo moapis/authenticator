ecf6ed5fcd2eba043e8575dee015ec9e49054e9f
Tim Möhlmann <muhlemmer@gmail.com>
2019-12-10 10:40:11 +0200

    SImple user list

---
+++ cmd/admin/config.go
@@ -0,0 +1,151 @@
+// Copyright (c) 2019, Mohlmann Solutions SRL. All rights reserved.
+// Use of this source code is governed by a License that can be found in the LICENSE file.
+// SPDX-License-Identifier: BSD-3-Clause
+
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"io/ioutil"
+	"strings"
+	"time"
+
+	"github.com/moapis/multidb"
+	pg "github.com/moapis/multidb/drivers/postgresql"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	log *logrus.Logger
+)
+
+func init() {
+	log = logrus.New()
+	log.SetLevel(logrus.InfoLevel)
+}
+
+// LogLevel used for logrus
+type LogLevel string
+
+const (
+	// PanicLevel sets logrus level to panic
+	PanicLevel LogLevel = "panic"
+	// FatalLevel sets logrus level to fatal
+	FatalLevel LogLevel = "fatal"
+	// ErrorLevel sets logrus level to error
+	ErrorLevel LogLevel = "error"
+	// WarnLevel sets logrus level to warn
+	WarnLevel LogLevel = "warn"
+	// InfoLevel sets logrus level to info
+	InfoLevel LogLevel = "info"
+	// DebugLevel sets logrus level to debug
+	DebugLevel LogLevel = "debug"
+	// TraceLevel sets logrus level to trace
+	TraceLevel LogLevel = "trace"
+)
+
+// TLSConfig for the gRPC server's CertFile and KeyFile
+type TLSConfig struct {
+	CertFile string `json:"certfile,omitempty"`
+	KeyFile  string `json:"keyfile,omitempty"`
+}
+
+// AuthServerConfig for the gRPC client connection
+type AuthServerConfig struct {
+	Host string
+	Port uint16
+}
+
+// ServerConfig is a collection on config
+type ServerConfig struct {
+	Addres      string           `json:"address"`     // HTTP listen Address
+	Port        uint16           `json:"port"`        // HTTP listen Port
+	AdminLTE    string           `json:"adminlte"`    // Path to AdminLTE root
+	Templates   string           `json:"templates"`   // Path to template directory
+	LogLevel    LogLevel         `json:"loglevel"`    // LogLevel used for logrus
+	TLS         *TLSConfig       `json:"tls"`         // TLS will be disabled when nil
+	AuthServer  AuthServerConfig `json:"authserver"`  // Config for the gRPC client connection
+	MultiDB     multidb.Config   `json:"multidb"`     // Imported from multidb
+	PG          *pg.Config       `json:"pg"`          // PG is later embedded in multidb
+	SQLRoutines int              `json:"sqlroutines"` // Amount of Go-routines for non-master queries
+}
+
+func (c *ServerConfig) writeOut(filename string) error {
+	out, err := json.MarshalIndent(c, "", "  ")
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(filename, out, 0644)
+}
+
+// Default confing
+var Default = ServerConfig{
+	Addres:    "127.0.0.1",
+	Port:      1234,
+	AdminLTE:  "AdminLTE",
+	Templates: "templates",
+	LogLevel:  DebugLevel,
+	TLS:       nil,
+	MultiDB: multidb.Config{
+		StatsLen:      100,
+		MaxFails:      10,
+		ReconnectWait: 10 * time.Second,
+	},
+	PG: &pg.Config{
+		Nodes: []pg.Node{
+			{
+				Host: "localhost",
+				Port: 5432,
+			},
+		},
+		Params: pg.Params{
+			DBname:          "authenticator",
+			User:            "postgres",
+			Password:        "",
+			SSLmode:         "disable",
+			Connect_timeout: 30,
+		},
+	},
+	SQLRoutines: 3,
+}
+
+var configFiles = flag.String("config", "", "Comma separated list of JSON config files")
+
+func configure(c ServerConfig) (*ServerConfig, error) {
+	flag.Parse()
+
+	files := strings.Split(*configFiles, ",")
+	s := &c
+	for _, f := range files {
+		if f == "" {
+			continue
+		}
+		log := log.WithField("file", f)
+		js, err := ioutil.ReadFile(f)
+		if err != nil {
+			log.WithError(err).Error("Read config file")
+			return nil, err
+		}
+		if err = json.Unmarshal(js, s); err != nil {
+			log.WithError(err).Error("Unmarshal config file")
+			return nil, err
+		}
+		log.Info("Applied config")
+	}
+
+	if s.PG != nil {
+		s.MultiDB.DBConf, s.PG = s.PG, nil
+	}
+
+	lvl, err := logrus.ParseLevel(string(s.LogLevel))
+	if err != nil {
+		return nil, err
+	}
+	log.WithField("level", lvl).Info("Setting log level")
+	log.SetLevel(lvl)
+
+	log.WithField("config", *s).Debug("Config loaded")
+
+	return s, nil
+}
--- cmd/admin/main.go
+++ cmd/admin/main.go
@@ -1,36 +1,123 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"html/template"
-	"log"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/moapis/authenticator/models"
+	"github.com/moapis/multidb"
+)
+
+func tmplPaths(names ...string) []string {
+	paths := make([]string, len(names))
+	for i, n := range names {
+		paths[i] = strings.Join([]string{conf.Templates, n}, "/")
+	}
+	return paths
+}
+
+type tmplData struct {
+	Content interface{} // Data for the "content" template
+}
+
+func isInternalError(w http.ResponseWriter, err error) bool {
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal server error"))
+		return true
+	}
+	return false
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles(tmplPaths("home.html", "base.html")...))
+	tmpl.ExecuteTemplate(w, "base", nil)
+}
+
+type action struct {
+	Name string
+	URL  string
+}
+
+type listEntry struct {
+	ID      int
+	Name    string
+	Created string
+	Updated string
+}
+
+const (
+	listDate = "_2 jan 06 15:04"
 )
 
 var (
-	adminlteDir  = flag.String("adminlte", "./AdminLTE", "directory with AdminLTE assets")
-	authServer   = flag.String("authServer", "127.0.0.1:8765", "Host and port for the authenticator server")
-	templateFile = flag.String("template", "templates/base.html", "HTML template file")
+	userActions = []string{
+		"view",
+		"reset password",
+		"delete",
+	}
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles(*templateFile))
-	tmpl.Execute(w, nil)
+func userList(ctx context.Context) ([]listEntry, error) {
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+	tx, err := mdb.MultiTx(ctx, nil, conf.SQLRoutines)
+	if err != nil {
+		return nil, err
+	}
+	users, err := models.Users().All(ctx, tx)
+	if err != nil {
+		return nil, err
+	}
+	list := make([]listEntry, len(users))
+	for i, u := range users {
+		list[i] = listEntry{
+			ID:      u.ID,
+			Name:    u.Name,
+			Created: u.CreatedAt.Format(listDate),
+			Updated: u.CreatedAt.Format(listDate),
+		}
+	}
+	return list, nil
 }
+
+func usersHandler(w http.ResponseWriter, r *http.Request) {
+	list, err := userList(r.Context())
+	if isInternalError(w, err) {
+		return
+	}
+	tmpl := template.Must(template.ParseFiles(tmplPaths("users.html", "base.html")...))
+	tmpl.ExecuteTemplate(w, "base", tmplData{Content: list})
+}
+
+var (
+	conf *ServerConfig
+	mdb  *multidb.MultiDB
+)
 
 func main() {
 	flag.Parse()
+	var err error
+	if conf, err = configure(Default); err != nil {
+		log.Fatal(err)
+	}
+	if mdb, err = conf.MultiDB.Open(); err != nil {
+		log.Fatal(err)
+	}
 
 	r := mux.NewRouter()
 
-	fs := http.FileServer(http.Dir(*adminlteDir))
+	fs := http.FileServer(http.Dir(conf.AdminLTE))
 	r.PathPrefix("/dist/").Handler(fs)
 	r.PathPrefix("/plugins/").Handler(fs)
 
 	r.HandleFunc("/", homeHandler)
+	r.HandleFunc("/users", usersHandler)
 	srv := &http.Server{
 		Handler:      r,
 		Addr:         "127.0.0.1:1234",
